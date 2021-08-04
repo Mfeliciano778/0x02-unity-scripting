ed17fdb2bc809301faf525e68dc5cd041c3de472
Mfeliciano778 <1652@holbertonschool.com>
2021-08-04 16:40:01 -0400

    Initial commit for task 6 - 7

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,11 +9,13 @@ public class PlayerController : MonoBehaviour
     Vector3 moveVec;
     public float speed = 15f;
     private int score;
+    public int health;
     // Start is called before the first frame update
     void Start()
     {
         character = GetComponent<CharacterController>();
         score = 0;
+        health = 5;
     }
 
     // Update is called once per frame
@@ -36,6 +38,11 @@ public class PlayerController : MonoBehaviour
             other.gameObject.SetActive(false);
             score += 1;
             Debug.Log($"Score: {score}");
+        }
+        if (other.gameObject.CompareTag("Trap"))
+        {
+            health -= 1;
+            Debug.Log($"Health: {health}");
         }
     }
 }
