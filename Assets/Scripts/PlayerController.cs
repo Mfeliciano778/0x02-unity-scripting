1c2d4c21264b5b592a89ad22d5a90c36742a5bb5
Mfeliciano778 <1652@holbertonschool.com>
2021-08-04 16:27:09 -0400

    Initial commit of tasks 1 - 5

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,10 +8,12 @@ public class PlayerController : MonoBehaviour
     CharacterController character;
     Vector3 moveVec;
     public float speed = 15f;
+    private int score;
     // Start is called before the first frame update
     void Start()
     {
         character = GetComponent<CharacterController>();
+        score = 0;
     }
 
     // Update is called once per frame
@@ -24,5 +26,16 @@ public class PlayerController : MonoBehaviour
     {
         Vector2 direction = context.ReadValue<Vector2>();
         moveVec = new Vector3(direction.x, 0, direction.y);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            // disable game object
+            other.gameObject.SetActive(false);
+            score += 1;
+            Debug.Log($"Score: {score}");
+        }
     }
 }
